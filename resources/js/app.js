c1f39a4230e95664b336d21d8ba1ac4309b536d3
Top Dreamer <128351913+topmsdreamer@users.noreply.github.com>
2023-10-30 19:01:46 -0500

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import './bootstrap';
+
+// custom select2
+$('#kt_datatable_search_status').select2();
+$('#kt_datatable_search_type').select2();
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PagesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('index');
+//});
+
+Route::get('/', [PagesController::class,'index']);
+
+
+Route::get('/datatables', [PagesController::class,'datatables']);
+Route::get('/ktdatatables', [PagesController::class,'ktDatatables']);
+Route::get('/select2', [PagesController::class,'select2']);
+Route::get('/icons/custom-icons', [PagesController::class,'customIcons']);
+Route::get('/icons/flaticon', [PagesController::class,'flaticon']);
+Route::get('/icons/fontawesome', [PagesController::class,'fontawesome']);
+Route::get('/icons/lineawesome', [PagesController::class,'lineawesome']);
+Route::get('/icons/socicons', [PagesController::class,'socicons']);
+Route::get('/icons/svg', [PagesController::class,'svg']);
+
+//
+//// Quick search dummy route to display html elements in search dropdown (header search)
+Route::get('/quick-search', [PagesController::class,'quickSearch'])->name('quick-search');
