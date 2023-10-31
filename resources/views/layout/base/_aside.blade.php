4ad7963ef5ced82bcf102e5a648117981af4d673
Top Dreamer <128351913+topmsdreamer@users.noreply.github.com>
2023-10-31 01:03:26 -0500

    Menu list updated

--- resources/views/layout/base/_aside.blade.php
+++ resources/views/layout/base/_aside.blade.php
@@ -16,7 +16,8 @@
     <div class="brand flex-column-auto {{ Metronic::printClasses('brand', false) }}" id="kt_brand">
         <div class="brand-logo">
             <a href="{{ url('/') }}">
-                <img alt="{{ config('app.name') }}" src="{{ asset('media/logos/'.$kt_logo_image) }}"/>
+{{--                <img alt="{{ config('app.name') }}" src="{{ asset('media/logos/'.$kt_logo_image) }}"/>--}}
+                <h1>TPM</h1>
             </a>
         </div>
 
