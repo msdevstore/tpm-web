80843a1fd72b2f7ce85356ba647c10ded93cdfce
Top Dreamer <128351913+topmsdreamer@users.noreply.github.com>
2023-11-03 11:10:53 -0500

    Admin dashboard design fixed

--- resources/views/admin/reports/pdf_generator.blade.php
+++ resources/views/admin/reports/pdf_generator.blade.php
@@ -20,18 +20,47 @@
                 <!--begin::Form-->
                 <form class="form">
                     <div class="card-body">
-                        <div class="row">
+                        <!--begin: Datatable-->
+                        <table class="table table-bordered table-separate table-head-custom table-foot-custom table-checkable text-center kt_datatable" style="margin-top: 13px !important">
+                            <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Mill</th>
+                                <th>Job Number</th>
+                                <th>Company Name</th>
+                                <th>Quantity</th>
+                                <th>Ordered Date</th>
+                                <th>Ship Date</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>5</td>
+                                <td>8791</td>
+                                <td>Halliburton</td>
+                                <td>100</td>
+                                <td>2023-07-12</td>
+                                <td>2023-07-12</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>6</td>
+                                <td>8791</td>
+                                <td>Halliburton</td>
+                                <td>100</td>
+                                <td>2023-07-12</td>
+                                <td>2023-07-12</td>
+                            </tr>
+                            </tfoot>
+                        </table>
+                        <!--end: Datatable-->
+                    </div>
+                    <div class="card-footer">
+                        <div class="row justify-content-end">
+                            <button type="reset" class="btn btn-success mr-2">Generate Report</button>
                         </div>
                     </div>
-                    {{--                    <div class="card-footer">--}}
-                    {{--                        <div class="row">--}}
-                    {{--                            <div class="col-lg-2"></div>--}}
-                    {{--                            <div class="col-lg-10">--}}
-                    {{--                                <button type="reset" class="btn btn-success mr-2">Submit</button>--}}
-                    {{--                                <button type="reset" class="btn btn-secondary">Cancel</button>--}}
-                    {{--                            </div>--}}
-                    {{--                        </div>--}}
-                    {{--                    </div>--}}
                 </form>
                 <!--end::Form-->
             </div>
